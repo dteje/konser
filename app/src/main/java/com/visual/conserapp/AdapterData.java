a884ae1ff283ab6baa91bfe4f0964797235a3f77
Unknown <ferraniusd@gmail.com>
2018-04-24 17:03:35 +0200

    .

--- app/src/main/java/com/visual/conserapp/AdapterData.java
+++ app/src/main/java/com/visual/conserapp/AdapterData.java
@@ -1,4 +1,48 @@
 package com.visual.conserapp;
 
-public class AdapterData {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AdapterData extends RecyclerView.Adapter<AdapterData.ViewHolderData>{
+
+    ArrayList<String> listData;
+
+    public AdapterData(ArrayList<String> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public ViewHolderData onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, null, false);
+        return new ViewHolderData(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolderData holder, int position) {
+        holder.assignData(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    public class ViewHolderData extends RecyclerView.ViewHolder {
+
+        TextView data;
+
+        public ViewHolderData(View itemView) {
+            super(itemView);
+            data = itemView.findViewById(R.id.idData);
+        }
+
+        public void assignData(String aData){
+            data.setText(aData);
+        }
+    }
 }
--- app/src/main/java/com/visual/conserapp/SandwitchCreator.java
+++ app/src/main/java/com/visual/conserapp/SandwitchCreator.java
@@ -2,13 +2,146 @@ package com.visual.conserapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class SandwitchCreator extends AppCompatActivity {
+
+    ArrayList<String> listData;
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandwitch_creator);
         setTitle("Sandwitch Creator");
+
+        recycler = (RecyclerView) findViewById(R.id.recyclerIngredientesId);
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        listData = new ArrayList<String>();
+
+        generateCarne();
+    }
+
+    public void buttonIngredient(View view){
+
+        switch(view.getId()){
+            case R.id.bMeat: generateCarne();
+                break;
+            case R.id.bVeggies: generateVerduras();
+                break;
+            case R.id.bCheese: generateQueso();
+                break;
+            case R.id.bSpecial: generateEspecial();
+                break;
+            case R.id.bSauces: generateSalsas();
+                break;
+        }
+
+    }
+
+    public void generateCarne (){
+
+        listData.clear();
+
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+        listData.add("Jamón York");
+
+        AdapterData adapter = new AdapterData(listData);
+        recycler.setAdapter(adapter);
+    }
+
+    public void generateVerduras (){
+
+        listData.clear();
+
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+        listData.add("Tomate");
+
+        AdapterData adapter = new AdapterData(listData);
+        recycler.setAdapter(adapter);
+    }
+
+    public void generateQueso (){
+
+        listData.clear();
+
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+        listData.add("Manchego");
+
+        AdapterData adapter = new AdapterData(listData);
+        recycler.setAdapter(adapter);
+    }
+
+    public void generateEspecial (){
+
+        listData.clear();
+
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+        listData.add("Huevo Frito");
+
+        AdapterData adapter = new AdapterData(listData);
+        recycler.setAdapter(adapter);
+    }
+
+    public void generateSalsas (){
+
+        listData.clear();
+
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+        listData.add("Mayonesa");
+
+        AdapterData adapter = new AdapterData(listData);
+        recycler.setAdapter(adapter);
     }
 }
