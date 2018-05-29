a08fb9be7895e0dcd677ed23f73bc391584fc435
Unknown <daniel.tejedor@outlook.com>
2018-05-29 17:28:17 +0200

    Añadida toolbar top a adminPanel

--- app/src/main/java/com/visual/conserapp/Views/AdminPanel.java
+++ app/src/main/java/com/visual/conserapp/Views/AdminPanel.java
@@ -1,10 +1,45 @@
 package com.visual.conserapp.Views;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.visual.conserapp.R;
 
 /**
  * Created by daniel on 29/05/2018.
  */
 
 public class AdminPanel extends AppCompatActivity {
-}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_panel);
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Panel del administrador");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.admin_top_menu, menu);
+        return true;
+    }
+
+    public boolean onNavSuperior(MenuItem menuitem) {
+        View view = menuitem.getActionView();
+        int id = menuitem.getItemId();
+        Intent intent;
+        if (id == R.id.admin_top_menu_item_client_view) intent = new Intent(this, Home.class);
+        else if (id == R.id.admin_top_menu_item_pending_requests) intent = new Intent(this, AdminHome.class);
+        else intent = new Intent(this, AdminPanel.class);
+        startActivity(intent);
+        return true;
+    }
+    }
