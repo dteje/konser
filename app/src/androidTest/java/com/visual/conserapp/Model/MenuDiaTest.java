812e358f0e746fa9a51b738295e108e8941d31e2
Unknown <hizfernandez@gmail.com>
2018-06-11 20:06:37 +0200

    MenuTest Creado

---
+++ app/src/androidTest/java/com/visual/conserapp/Model/MenuDiaTest.java
@@ -0,0 +1,66 @@
+package com.visual.conserapp.Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuDiaTest {
+
+    MenuDia menuDia;
+    @Before
+    public void setUp(){
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getPrice() {
+    }
+
+    @Test
+    public void setPrice() {
+    }
+
+    @Test
+    public void getListaPlatos() {
+    }
+
+    @Test
+    public void setListaPlatos() {
+    }
+
+    @Test
+    public void getIdsPlato1() {
+    }
+
+    @Test
+    public void setIdsPlato1() {
+    }
+
+    @Test
+    public void getIdsPlato2() {
+    }
+
+    @Test
+    public void setIdsPlato2() {
+    }
+
+    @Test
+    public void getIdsPlato3() {
+    }
+
+    @Test
+    public void setIdsPlato3() {
+    }
+
+    @Test
+    public void getPlatosOrdenados() {
+    }
+}
